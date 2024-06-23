0974a2b3f90205fec89521de03c23031d39cc8c6
Shreya Tiwari <125819373+TShreya-27@users.noreply.github.com>
2024-06-23 18:20:54 +0530

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -10,27 +10,15 @@ export default function Navbar() {
         <Link className="navbar-brand" to="/">
           <img src={logo} alt="Time-Table Icon" width="200" height="80" />
         </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-          style={{ color: "#FEFEFA" }}
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/" style={{ color: "#FEFEFA" }}>
+              <Link className="nav-link active" aria-current="page" to="/Homepage.js" style={{ color: "#FEFEFA" }}>
                 <span className="material-icons">home</span>
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link active" to="/timetable" style={{ color: "#FEFEFA" }}>
+              <Link className="nav-link active" to="/Timetable.js" style={{ color: "#FEFEFA" }}>
                 <span className="material-icons">schedule</span>
               </Link>
             </li>
