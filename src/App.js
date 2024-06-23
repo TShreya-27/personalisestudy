d1a8948fe198ded3cd478566c439ade50685875e
Shreya Tiwari <125819373+TShreya-27@users.noreply.github.com>
2024-06-23 17:56:21 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./Navbar";
-import Footer from "./Footer";
 import Homepage from "./Homepage";
 import Timetable from "./Timetable";
 
 function App() {
   return (
     <Router>
-      <div>
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={Homepage} />
-          <Route path="/Homepage" component={Homepage} />
-          <Route path="/Timetable" component={Timetable} />
-        </Switch>
-        <Footer />
-      </div>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/timetable" component={Timetable} />
+      </Switch>
     </Router>
   );
 }
