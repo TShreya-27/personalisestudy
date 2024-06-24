5d7d70282c71a2c9527a4f6b37257e18ea0d68d3
Shreya Tiwari <125819373+TShreya-27@users.noreply.github.com>
2024-06-24 17:10:06 +0530

    update

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg" style={{ backgroundColor: "#F7CAC9" }}>
+    <nav className="navbar navbar-expand-lg" style={{ backgroundColor: "#90c2e7" }}>
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
           <img src={logo} alt="Time-Table Icon" width="200" height="80" />
@@ -13,12 +13,12 @@ export default function Navbar() {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/Homepage" style={{ color: "#FEFEFA" }}>
+              <Link className="nav-link active" aria-current="page" to="/Homepage" style={{ color: "#e7dfe8" }}>
                 <span className="material-icons">home</span>
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link active" to="/Timetable" style={{ color: "#FEFEFA" }}>
+              <Link className="nav-link active" to="/Timetable" style={{ color: "#e7dfe8" }}>
                 <span className="material-icons">schedule</span>
               </Link>
             </li>
