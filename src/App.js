d2dec38a070f906557469a1042441c3440ecb5b1
Shreya Tiwari <125819373+TShreya-27@users.noreply.github.com>
2024-06-24 16:27:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import { Switch } from 'react-router-dom';
 import Navbar from "./Navbar";
 import Homepage from "./Homepage";
 import Timetable from "./Timetable";
